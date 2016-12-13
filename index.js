aad49e6f361740d7794d93feef0837b9890a9023
Xuebing Wang <xbwang93@gmail.com>
2016-12-13 03:59:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+var evens = [0, 2, 4, 6, 8, 10]
+ 
+evens.forEach((even, index, array) => {
+  debugger
+  console.log(`${even} is not odd!`)
+})
+
+function square(n) {
+  console.log(n * n)
+}
+
+function doToEvens (callback){
+  evens.forEach(callback)
+}
+
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array){
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+var animals = ['dog', 'fish', 'cat']
+
+doToElementsInArray(animals, changeCompletely)
+
+console.log(animals)
